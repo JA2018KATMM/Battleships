daa80531d27a69420c982298663cdf0f48fc3af4
Marta Staporek <marta_staporek@epam.com>
2018-12-12 16:25:37 +0100

    JavaFX Pusta okienkowa aplikacja jest poprawnie pakowana do jara. Poprawione wcięcia w kodzie.  Please enter the commit message for your changes. Lines starting

--- client/src/main/java/academy/konrad/group/battleships/App.java
+++ client/src/main/java/academy/konrad/group/battleships/App.java
@@ -1,7 +1,7 @@
 package academy.konrad.group.battleships;
 
 public class App {
-    public static void main(String[] args) {
-        Main.main(args);
-    }
+  public static void main(String[] args) {
+    Main.main(args);
+  }
 }
--- client/src/main/java/academy/konrad/group/battleships/Main.java
+++ client/src/main/java/academy/konrad/group/battleships/Main.java
@@ -6,24 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.net.URL;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
-//        URL url = new File("client/src/main/resources/sample.fxml").toURI().toURL();
-//        FXMLLoader loader = new FXMLLoader(url);
-//        Parent root = loader.load();
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
+    primaryStage.setTitle("Hello World");
+    primaryStage.setScene(new Scene(root, 300, 275));
+    primaryStage.show();
+  }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
