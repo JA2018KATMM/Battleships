47cd9102b0974bafe907f8e676aea165aa40cb76
Mateusz Syposz <mat.syposz@gmail.com>
2018-12-13 10:47:43 +0100

    Servlet, budowa do war i skrypt wdrożenia na produkcje. (#23)
    servlet, pakowanie w wara i wdrozenie na tomcat,
    dodanie watku ktory nasluchuje w tle polaczenia do socketu

--- server/src/main/java/academy/konrad/group/battleships/server/ServerApp.java
+++ server/src/main/java/academy/konrad/group/battleships/server/ServerApp.java
@@ -1,45 +1,20 @@
 package academy.konrad.group.battleships.server;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.PrintWriter;
 
-class ServerApp {
+public class ServerApp extends HttpServlet {
 
-  private static final int PORT_NUMBER = 8081;
-  private static final LoggedClientsSet LOGGED_CLIENTS_SET = new LoggedClientsSet();
-
-  public static void main(String[] args) {
-    ServerSocket serverSocket = null;
-
-    try {
-      serverSocket = new ServerSocket(PORT_NUMBER);
-    } catch (IOException exception) {
-      System.out.println("Exception caught when trying to listen on port "
-          + PORT_NUMBER + " or listening for a connection");
-      exception.printStackTrace();
+    public void init() {
+      Thread listenerThread = new ListenerThread();
+      listenerThread.start();
     }
 
-    boolean shouldContinue = true;
-
-    while (shouldContinue) {
-      try {
-        Socket clientSocket = serverSocket.accept();
-        DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
-        DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
-
-        LoggedClient loggedClient = new LoggedClient(clientSocket, dis, dos, LOGGED_CLIENTS_SET);
-        loggedClient.inform("Witaj w grze statki!");
-        LOGGED_CLIENTS_SET.addClient(loggedClient);
-
-        Thread thread = new Thread(loggedClient);
-        thread.start();
-      } catch (IOException exception) {
-        exception.printStackTrace();
-        shouldContinue = false;
-      }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        out.print("Battleship says hello!");
     }
-  }
 }
