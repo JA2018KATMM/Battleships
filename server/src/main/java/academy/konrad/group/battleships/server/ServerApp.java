7ecbcb185fa6379172e96d72001ac72629cf5273
Konrad Kusiak <konrad_kusiak@epam.com>
2018-12-18 10:09:32 +0100

    simple website where game client can be downloaded

--- server/src/main/java/academy/konrad/group/battleships/server/ServerApp.java
+++ server/src/main/java/academy/konrad/group/battleships/server/ServerApp.java
@@ -1,20 +1,21 @@
 package academy.konrad.group.battleships.server;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class ServerApp extends HttpServlet {
 
-    public void init() {
-      Thread listenerThread = new ListenerThread();
-      listenerThread.start();
-    }
+  @Override
+  public void init() {
+    Thread listenerThread = new ListenerThread();
+    listenerThread.start();
+  }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        PrintWriter out = response.getWriter();
-        out.print("Battleship says hello!");
-    }
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    request.getRequestDispatcher("index.jsp").forward(request, response);
+  }
 }
