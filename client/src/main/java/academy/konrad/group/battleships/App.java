0cc7b0fd1dda585e1d3cf2d380f4305cf2943dd3
Adam Michalak <adam_michalak@epam.com>
2019-02-06 07:03:57 +0100

    Rozpoczęcie pracy nad modułem klienta

--- client/src/main/java/academy/konrad/group/battleships/App.java
+++ client/src/main/java/academy/konrad/group/battleships/App.java
@@ -1,10 +1,8 @@
 package academy.konrad.group.battleships;
 
-import academy.konrad.group.battleships.userinterface.Main;
-
 public class App {
 
   public static void main(String[] args) {
-    Main.main(args);
+      Thread.currentThread().setName("ClientMainThread");
   }
 }
